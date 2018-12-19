aec0777bf9b9127e5ed17aada6eade3d2c8186af
Tomer <tomash22123@gmail.com>
2018-12-19 20:23:45 +0200

    First commit

---
+++ Main.py
@@ -0,0 +1,9 @@
+"""
+The main class that will run the different modules of the assignment
+"""
+
+if __name__ == '__main__':
+
+    corpus_path = 'D:\\20_newsgroups'  # PATH TO THE FOLDER THAT CONTAINS THE SUB-FOLDERS
+
+    
---
+++ Reader.py
@@ -0,0 +1,3 @@
+"""
+Class that reads the files
+"""
