289ce30c869f188e71d3021d678a7c7dbdd3298c
Shaked Eyal <shaked.eyal@gmail.com>
2018-12-30 16:44:56 +0200

    Stated classification and feature extracting

--- Main.py
+++ Main.py
@@ -5,13 +5,17 @@ from preprocessor import PreProcessor
 from Classifier import Classifier
 
 if __name__ == '__main__':
-    corpus_path = 'D:\\ohsumed-first-20000-docs'  # PATH TO THE CORPUS FOLDER THAT CONTAINS THE SUB-FOLDERS
-    output_path = 'D:\\parsed_files'  # Output path where the statistics and parsed files will be saved
+    # corpus_path = '.\\ohsumed-first-20000-docs'  # PATH TO THE CORPUS FOLDER THAT CONTAINS THE SUB-FOLDERS
+    # output_path = '.\\output'  # Output path where the statistics and parsed files will be saved
 
     # prep = PreProcessor(corpus_path, output_path)
     # prep.process_all_files()
 
-    # prep.load_statistics('D:\\parsed_files\\statistics')
+    # prep.load_statistics('.\\statistics')
     # prep.print_statistics()
 
-    classifer = Classifier(train_data_path='D:\\ohsumed-first-20000-docs')
+    classifer = Classifier(courpus_path='D:\\ohsumed-first-20000-docs')
+    tf_idf_train, tf_idf_test = classifer.tf_idf_feature_extraction()
+    bigram_train, bigram_test = classifer.bigram_feature_extraction()
+    hash_train, hash_test = classifer.hash_feature_extraction()
+
